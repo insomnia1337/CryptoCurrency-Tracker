4ac6ce65465bad18f4f62723222db4dc1b93d0f7
insomnia1337 <mateusz.bieda95@gmail.com>
2018-02-23 17:48:26 +0100

    Delete unnecessary content

---
+++ src/components/CoinList/CoinList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './CoinList.css';
+
+class CoinList extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default CoinList;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './SearchBar.css';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Wel</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
